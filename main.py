8eb84083e9b8edb46b2c71d25595ed3052c5a0a4
Z-fly <10470892+Z-fly@users.noreply.github.com>
2025-10-03 19:02:35 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 import code
+import sys
+import os
 
 def main():
+
+    print(__compiled__.containing_dir,1111,'\n')
+    print(os.path.dirname(sys.argv[0]),2222,'\n')
+    print(__file__,3333,'\n')
+    
     
     banner = "模拟 Python 控制台，输入 exit() 退出"
     console = code.InteractiveConsole()
@@ -8,3 +15,4 @@ def main():
 
 if __name__ == '__main__':
     main()
+
